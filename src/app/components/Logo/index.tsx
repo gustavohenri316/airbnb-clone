ab9826013f859b0b2ca519bee6a433c86f5d1910
Gustavo <gustavo.henrique@gooxxy.com>
2023-05-13 09:57:22 -0300

    first

---
+++ src/app/components/Logo/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export const Logo: React.FC = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <Image
+        src="/images/logo.png"
+        className="hidden md:block cursor-pointer"
+        width='100'
+        height='100'
+        alt="logo"
+      />
+    </div>
+  );
+  return <div>oi</div>;
+};
